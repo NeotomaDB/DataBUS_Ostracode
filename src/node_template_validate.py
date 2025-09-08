11ec08b08cb6a764631e13e63232efc8552718db
Socorro Dominguez <seiryu88@icloud.com>
2025-09-08 11:47:19 -0700

    BU-254 Making sure uv structure of DataBUS is not interferring and works properly with Ostracodes NODE

--- src/node_template_validate.py
+++ src/node_template_validate.py
@@ -69,7 +69,7 @@ for filename in filenames:
             logfile = logging_response(validator['analysisunit'], logfile)
             
             logfile.append('\n === Checking Chronologies ===')
-            validator['chronologies'] = nv.valid_chronologies(**inputs)
+            validator['chronologies'] = nv.valid_chronologies(**inputs, multiple=False)
             logfile = logging_response(validator['chronologies'], logfile)
 
             logfile.append('\n === Checking Dataset ===')
